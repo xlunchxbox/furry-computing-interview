d2da0ef96c6acf4849e8cfdfb67877507b2e8b79
Adam Strickland <adam.strickland@gmail.com>
2012-11-06 13:51:31 -0600

    initial rails app

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'sqlite3'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'rspec-rails', '~> 2.4'
+  gem 'faker'
+  gem 'cucumber'
+  gem 'cucumber-rails', '~. 1.0', require: false
+  gem 'database_cleaner'
+  gem 'capybara'
+  gem 'shoulda', require: false
+  gem 'factory_girl_rails', '~> 3.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlowingCyrilInterview::Application.config.secret_token = '8bec0d38a910ad39c00d4df3f382edf946787c6780c61095519423da7cfac0bddba26ae5ee9b778fd407639dba614cc6458b80b642e71e51ea3cf428105922ac'
